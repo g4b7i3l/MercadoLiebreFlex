8e63242bae8dae03960bc6bfcd85b5db5dceb263
g4b7i3l <gabiibagna@gmail.com>
2021-07-09 18:08:23 -0300

    agregando formularios

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.use(express.static('public'));
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'/views',"home.html"))); //ruta raiz localhost:3000
 app.get('/contactos',(req,res)=> res.send('Página de contacto'));
 app.get('/productos',(req,res)=> res.send('Listado de productos'));
+app.get('/registro',(req,res)=>res.sendFile(path.join(__dirname,'/views',"registro.html")));
+app.get('/login',(req,res)=>res.sendFile(path.join(__dirname,'/views',"login.html")));
+
 
 app.listen(port,() => console.log("Servidor funcionando en http://localhost:"+ port )) // 3000 es el numero de puerto - seguido de un callback
 
